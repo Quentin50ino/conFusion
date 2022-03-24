6326e85cbb6fe61958ea94bc94d417a0bb33eb6b
Alberto Bollino <alberto.bollino@outlook.it>
2022-03-25 00:56:17 +0100

    add menu component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core'; //importa la classe Component da angular core
 
+//un componente contiene dei metadati (selector, template, style), proprietà e metodi (che stanno all'interno della classe)
+
 //Component decorator
 @Component({
   selector: 'app-root', //è l'app-root presente nell'index.html!
   templateUrl: './app.component.html', //punta al file che contiene il template di questo particolare componente
+  //template: `<h1>{{title}}</h1>`, questo è chiamato inline template (si usa solo quando il template è molto semplice da scrivere)
   styleUrls: ['./app.component.css'] //punta al file che contiene il css di questo componente
 })
-export class AppComponent {
+
+//il Componenet non è nient'altro che una classe javascript (o meglio TypeScript)
+export class AppComponent { //la classe può essere esportata
   title = 'conFusion'; //variabile di classe
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,25 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material';
 
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component'; //è aggiunto autocamicamente ogni volta che generiamo un componente da Angular CLI
 
 //NgModule decorator
 @NgModule({
   declarations: [
-    AppComponent //dichiara che questo componente appartiene a questo modulo
+    AppComponent,
+    MenuComponent //dichiara che questo componente appartiene a questo modulo
   ],
   imports: [
     BrowserModule, //dichiara quali import sono necessari per questo modulo
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
+    MatListModule
   ],
   providers: [], //specifica tutti i services che servono per questo modulo
   bootstrap: [AppComponent]
