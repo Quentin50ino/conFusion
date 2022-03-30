214864359ed950971ff7d2849d46178ae6e2baf6
Alberto Bollino <alberto.bollino@outlook.it>
2022-03-31 01:40:27 +0200

    rxjs part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,7 @@ import { Dish } from '../shared/dish';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { DishService } from '../services/dish.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dishdetail',
@@ -14,16 +15,28 @@ export class DishdetailComponent implements OnInit {
   //@Input() dish: Dish; //è la variabile che viene passata all'elemento <app-dishdetail> all'interno del menu.component
   //per indicare che viene presa dall'esterno si utilizza il decorator @Input
   dish: Dish;
+  dishIds : string[];
+  prev: string;
+  next: string;
 
   constructor(private dishService: DishService,
     private route: ActivatedRoute,
     private location: Location) { }
 
   ngOnInit() {
-    let id = this.route.snapshot.params['id'];
-    console.log(id);
+    this.dishService.getDishIds()
+    .subscribe((dishIds) => this.dishIds = dishIds ); 
+    this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
+    .subscribe((dish) => {this.dish = dish; this.setPrevNext(dish.id);});
+    //console.log(id);
     //this.dish = this.dishService.getDish(id);
-    this.dishService.getDish(id).subscribe((dish) => this.dish = dish);
+    //this.dishService.getDish(id).subscribe((dish) => this.dish = dish);
+  }
+
+  setPrevNext(dishId: string){
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
 
   goBack(): void {
